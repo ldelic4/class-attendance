22105d53df306c2c2d9bd9d4309710dfb264f841
Lea Delic <leadelic0429@gmail.com>
2024-12-02 18:04:57 -0500

    hey

---
+++ script.js
@@ -0,0 +1,27 @@
+const ctx= document.getElementById('barChart');
+
+new Chart(
+    ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow","Black"],
+            dataset:[
+                {
+                    label: "Votes",
+                    data: [12,19,3,10],
+                    backGroundColor: [
+                        'rgba(245, 40, 145, 0.8)',
+                         'rgba(57, 177, 145, 0.8)', 
+                         'rgba(223, 207, 37, 0.8)',
+                        'rgba(43, 207, 37, 0.8)'],
+                    borderColor: [
+                            'rgba(245, 40, 145)',
+                             'rgba(57, 177, 145)', 
+                             'rgba(223, 207, 37)',
+                            'rgba(43, 207, 37)'],
+                }
+            ]
+        }
+    }
+
+)
